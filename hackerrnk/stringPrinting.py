febd909ca18bc4066a3069ac1ca0735fff72bd07
Swachchand <umeshkalkundrikar@gmail.com>
2019-05-22 13:12:16 -0700

    Update string slicing per the definition

--- hackerrnk/stringPrinting.py
+++ hackerrnk/stringPrinting.py
@@ -6,23 +6,28 @@ to N-1, print its even-indexed and odd-indexed characters
  as  2 space-separated strings on a single line
 """
 
-def even_bits(str):
+def even_bits(str1):
     result = ""  
     for i in range(len(str)):
         if i % 2 == 0:
             result = result + str[i]
     return result
-def odd_bits(str):
+def odd_bits(str1):
     result = ""  
-    for i in range(len(str)):
+    for i in range(len(str1)):
         if i % 2 == 1:
             result = result + str[i]
     return result
 
+ip_lst = [] # input list that contains all input strings
 
 for i in range(int(input())):
-    w = input('')    
-    print(even_bits(w), ' ' ,odd_bits(w))
+    w = input('')
+    ##print(w[::2], w[1::2])
+    ip_lst.append(w)
+
+for inp in ip_lst:
+    print(even_bits(inp), ' ', odd_bits(inp))
 
 #############################################
 """
