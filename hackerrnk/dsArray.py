d953cf80d2f365b02f4e3cf76881aa1549816a39
Swachchand <umeshkalkundrikar@gmail.com>
2019-06-04 12:54:34 -0700

    Print the elements of array  in reverse order as a single line of space-separated numbers

--- hackerrnk/dsArray.py
+++ hackerrnk/dsArray.py
@@ -1,14 +1,25 @@
-
 '''
-Print the elements of array A in reverse order as a single 
-line of space-separated numbers.
-
+Task 
+Given an array,N , of  integers, print N's elements in reverse order as a single line of space-separated numbers.
+Input Format
+The first line contains an integer,  N (the size of our array). 
+The second line contains N space-separated integers describing array A's elements.
+Constraints
+1<=N<=1000
+1<Ai<1000, where Ai is the ith integer in the array.
+Sample Input
+4
+1 4 3 2
+Sample Output
+2 3 4 1
+Print the elements of array  in reverse order as a single line of space-separated numbers.
 '''
 
 n= int(input())
 arr = []
-### Have the array typed in 1 line using space
+###Have an array typed in 1 line by following statement
 arr = list(map(int, input().split()))
 
-## Reverse array by converting it to MAP and printing it
+## Reverse the array and print it converting to Map
+
 print(" ".join(map(str, arr[::-1])))
