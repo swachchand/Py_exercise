17007243c8c1ff722bf9215c5268deecdc3635b3
Swachchand <umeshkalkundrikar@gmail.com>
2019-06-05 14:43:08 -0700

    Print entire character stream in reverse

---
+++ python_small_examples/pythonSnippets/revString.py
@@ -0,0 +1,28 @@
+'''
+Print entire character stream in reverse 
+hello world 
+olleh dlrow
+'''
+
+
+def reverseWordSentence(Sentence): 
+
+    # Spliting the Sentence into list of words. 
+    words = Sentence.split(" ") 
+    
+    # Reversing each word and creating 
+    # a new list of words 
+    # List Comprehension Technique 
+    newWords = [word[::-1] for word in words] 
+    
+    # Joining the new list of words 
+    # to for a new Sentence 
+    newSentence = " ".join(newWords) 
+
+    return newSentence 
+
+# Driver's Code 
+Sentence = input('Enter String: ')
+# Calling the reverseWordSentence 
+# Function to get the newSentence 
+print(reverseWordSentence(Sentence))
