a2643e4e26ca297ca34234d0804ad217b5e11360
Umesh Kalkundrikar <umeshkalkundrikar@Umeshs-MacBook-Pro.local>
2019-06-22 14:15:55 -0700

    just a scratch pad

--- python_small_examples/pythonSnippets/scratchPad.py
+++ python_small_examples/pythonSnippets/scratchPad.py
@@ -1,4 +1,38 @@
 ## This file is a scratchpad can be used and changed as per practice codes
+import re
+print('Testing all patterns of REGEX expression')
 
-print('hello world')
- 
+rgx = re.compile(r'\d\d\d-\d\d\d-\d\d\d')
+rgxGrp = re.compile(r'\(\d\d\d\) (\d\d\d-\d\d\d\d)')
+inpt= input('Enter an input:')
+
+opt = rgx.search(inpt)
+print(opt.group())
+
+opt1 = rgxGrp.search(inpt)
+print(opt1.group())
+
+
+'''
+Working on
++ * and ? patterns
+? --> Pattern appears 0 or 1 time
+* --> Pattern appears zero or more times
++ --> Pattern appears 1 or more times
+
+'''
+btrx = re.compile(r'mat(wo)?man') # For zero or 1 more time
+btrx1 = re.compile(r'bat(wo)*man') # zero or more times
+btrx3 = re.compile(r'bat(wo)+man') # zero or more times
+
+
+
+opt2= btrx.search(inpt)
+print(opt2.group())
+
+opt3= btrx1.search(inpt)
+print(opt3.group())
+
+
+opt4= btrx3.search(inpt)
+print(opt4.group())
