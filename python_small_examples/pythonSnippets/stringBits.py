6dd7c34d8db9b34913737a284b9ce2c057ff1534
Swachchand <umeshkalkundrikar@gmail.com>
2019-07-03 11:56:04 -0700

    String bits for a given word/s

---
+++ python_small_examples/pythonSnippets/stringBits.py
@@ -0,0 +1,17 @@
+'''
+
+Given a string, 
+return a new string made of every other char starting 
+with the first, so "Hello" yields "Hlo".
+
+string_bits('Hello') --'Hlo'
+string_bits('Hi') -- 'H'
+string_bits('Heeololeo') -- 'Hello'
+
+'''
+
+def string_bits(st):
+  return(st[::2])
+  
+s1 = input ('Enter String: ')
+print(string_bits(s1))
